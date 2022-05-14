9b334954be0d230bdf2fc2462682cc5872834972
Luis Dudamel <ladusa@gmail.com>
2022-05-14 22:18:01 +0200

    Added cors package

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const { error404NotFound, general500Error } = require("./middlewares/errors");
 const { robotsRouter } = require("./router/robotsRouter");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
